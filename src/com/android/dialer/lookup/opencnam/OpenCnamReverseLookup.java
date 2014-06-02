649f94749bffc2eba34fa742edb05868d15524a1
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-06-02 18:30:59 +0100

    ReverseLookup: Don't return OpenCNAM errors as caller IDs
    Skip the API entirely for non-US numbers, and ignore any non-200
    bodies to avoid names of the "Limit Exceeded" variety
    
    Change-Id: I1ea107ba828e6dba44fa0828ad5fedecb8faf4de

--- src/com/android/dialer/lookup/opencnam/OpenCnamReverseLookup.java
+++ src/com/android/dialer/lookup/opencnam/OpenCnamReverseLookup.java
@@ -57,6 +57,12 @@ public class OpenCnamReverseLookup extends ReverseLookup {
     public Pair<ContactInfo, Object> lookupNumber(Context context,
             String normalizedNumber, String formattedNumber) {
         String displayName;
+
+        if (normalizedNumber != null && normalizedNumber.startsWith("+") &&
+            !normalizedNumber.startsWith("+1")) {
+             // Any non-US number will return "We currently accept only US numbers"
+             return null;
+        }
         try {
             displayName = httpGetRequest(normalizedNumber);
             if (DEBUG) Log.d(TAG, "Reverse lookup returned name: " + displayName);
@@ -98,6 +104,10 @@ public class OpenCnamReverseLookup extends ReverseLookup {
         HttpGet request = new HttpGet(LOOKUP_URL + number);
 
         HttpResponse response = client.execute(request);
+
+        if (response.getStatusLine().getStatusCode() != 200) {
+            throw new IOException();
+        }
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         response.getEntity().writeTo(out);
