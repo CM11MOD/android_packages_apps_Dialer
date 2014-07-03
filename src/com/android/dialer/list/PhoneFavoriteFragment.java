7c499bef2e652e8f70df94a31dfc8d18167c1a89
Danesh M <daneshm90@gmail.com>
2014-07-02 20:57:34 -0700

    Dialer : Don't hold on to views used in listviews
    Lets not hold references to views used inside of a listview adapter since it
    will try to recycle it and if something is holding a reference to that view it is
    trying to recycle, it will block things on the ui thread and queue up all events during that time
    and only dispatch once focus is lost.
    
    Change-Id: Idd113e4f13f08d706a33fd4d07fd9a12f5382671
    
    Conflicts:
    	src/com/android/dialer/list/PhoneFavoriteFragment.java

--- src/com/android/dialer/list/PhoneFavoriteFragment.java
+++ src/com/android/dialer/list/PhoneFavoriteFragment.java
@@ -197,8 +197,6 @@ public class PhoneFavoriteFragment extends Fragment implements OnItemClickListen
 
     private PhoneFavoriteListView mListView;
 
-    private View mPhoneFavoritesMenu;
-
     private TileInteractionTeaserView mTileInteractionTeaserView;
 
     private final HashMap<Long, Integer> mItemIdTopMap = new HashMap<Long, Integer>();
@@ -288,14 +286,11 @@ public class PhoneFavoriteFragment extends Fragment implements OnItemClickListen
 
         mEmptyView = mParentView.findViewById(R.id.phone_no_favorites_view);
 
-        mPhoneFavoritesMenu = inflater.inflate(R.layout.phone_favorites_menu, mListView, false);
-        prepareFavoritesMenu(mPhoneFavoritesMenu);
-
         mTileInteractionTeaserView = (TileInteractionTeaserView) inflater.inflate(
                 R.layout.tile_interactions_teaser_view, mListView, false);
 
         mAdapter = new PhoneFavoriteMergedAdapter(getActivity(), this, mContactTileAdapter,
-                mCallLogAdapter, mPhoneFavoritesMenu, mTileInteractionTeaserView);
+                mCallLogAdapter, mTileInteractionTeaserView);
 
         mTileInteractionTeaserView.setAdapter(mAdapter);
 
@@ -652,20 +647,5 @@ public class PhoneFavoriteFragment extends Fragment implements OnItemClickListen
         prefs.edit().putLong(KEY_LAST_DISMISSED_CALL_SHORTCUT_DATE, mLastCallShortcutDate)
                 .apply();
         fetchCalls();
-    }
-
-    /**
-     * Prepares the favorites menu which contains the static label "Speed Dial" and the
-     * "All Contacts" button.  Sets the onClickListener for the "All Contacts" button.
-     */
-    private void prepareFavoritesMenu(View favoritesMenu) {
-        Button allContactsButton = (Button) favoritesMenu.findViewById(R.id.all_contacts_button);
-        // Set the onClick listener for the button to bring up the all contacts view.
-        allContactsButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showAllContacts();
-            }
-        });
     }
 }
